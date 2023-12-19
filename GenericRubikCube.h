5b28fa6826e3ebb5dc9dcf13addff4bc0e6d128a
Shreyas Saxena <shreyasflash52@gmail.com>
2023-12-19 13:48:27 +0530

    Updated GenericCube

--- GenericRubikCube.h
+++ GenericRubikCube.h
@@ -35,7 +35,58 @@ public:
         B, Bp, B2
     };
     virtual COLOR getColor(FACE face, unsigned row, unsigned col) const = 0;
+
+    static char getColorLetter(COLOR color);
+
     virtual bool isSolved() const = 0;
+
+    static string getMove(MOVE ind);
+
+    void print() const;
+
+    vector<MOVE> randomShuffleCube(unsigned int times);
+
+    GenericRubikCube &move(MOVE ind);
+
+    GenericRubikCube &invert(MOVE ind);
+
+    virtual GenericRubikCube &f() = 0;
+
+    virtual GenericRubikCube &fPrime() = 0;
+
+    virtual GenericRubikCube &f2() = 0;
+
+    virtual GenericRubikCube &u() = 0;
+
+    virtual GenericRubikCube &uPrime() = 0;
+
+    virtual GenericRubikCube &u2() = 0;
+
+    virtual GenericRubikCube &l() = 0;
+
+    virtual GenericRubikCube &lPrime() = 0;
+
+    virtual GenericRubikCube &l2() = 0;
+
+    virtual GenericRubikCube &r() = 0;
+
+    virtual GenericRubikCube &d() = 0;
+
+    virtual GenericRubikCube &dPrime() = 0;
+
+    virtual GenericRubikCube &d2() = 0;
+
+    virtual GenericRubikCube &rPrime() = 0;
+
+    virtual GenericRubikCube &r2() = 0;
+
+    virtual GenericRubikCube &b() = 0;
+
+    virtual GenericRubikCube &bPrime() = 0;
+
+    virtual GenericRubikCube &b2() = 0;
+
+
 
 
 };
