7a363bf058fee842ca0bbfed66e04f253d751c37
Shreyas Saxena <shreyasflash52@gmail.com>
2023-12-20 18:28:57 +0530

    Updated GenericCube

--- main.cpp
+++ main.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 
 int main() {
-    RubikCube3DArray obj3DArray;
-    vector<GenericRubikCube::MOVE> movesinshuffle = obj3DArray.randomShuffleCube(3);
-    for(auto move:movesinshuffle)
-    {
-        cout<<obj3DArray.getMove(move)<<" ";
-    }
-    cout<<"\n";
-    obj3DArray.print();
+    RubikCube3DArray obj1DArray;
+    obj1DArray.print();
     return 0;
 }
