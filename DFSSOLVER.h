1ae82cc473080e964edcfc34b60423ca0a0f221f
Shreyas Saxena <shreyasflash52@gmail.com>
2023-12-23 13:27:04 +0530

    Updated GenericCube

--- DFSSOLVER.h
+++ DFSSOLVER.h
@@ -9,6 +9,42 @@
 #include "GenericRubikCube.h"
 
 template<typename T, typename H>
-
+class DFSSOLVER{
+private:
+    vector<GenericRubikCube::MOVE> moves;
+    int max_search_depth;
+    int k = 0;
+    void dfs(int depth)
+    {
+        if(k==1)return;
+        if(rubiksCube.isSolved())
+        {
+            for(auto move:moves)cout<<rubiksCube.getMove(move)<<" ";
+            k = 1;
+            return;
+        }
+        for(int i=0;i<18;i++)
+        {
+            auto cur_move = GenericRubikCube::MOVE(i);
+            rubiksCube.move(cur_move);
+            moves.push_back(cur_move);
+            if(depth<max_search_depth)dfs(depth+1);
+            rubiksCube.invert(cur_move);
+            moves.pop_back();
+        }
+    }
+public:
+    T rubiksCube;
+    DFSSOLVER(T rc, int md)
+    {
+        rubiksCube = rc;
+        max_search_depth = md;
+    }
+    vector<GenericRubikCube::MOVE> solve()
+    {
+        dfs(0);
+        return moves;
+    }
+};
 
 #endif //RUBIKCUBEPROJECT_DFSSOLVER_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
 #include "RubikCube3DArray.cpp"
+#include "DFSSOLVER.h"
 using namespace std;
 int main() {
-
-
+    RubikCube3DArray cube;
+    vector<GenericRubikCube::MOVE> shuffle_moves = cube.randomShuffleCube(4);
+    for(auto move:shuffle_moves)cout<<cube.getMove(move)<<" ";
+    cout<<"\n";
+    cube.print();
+    cout<<"\n";
+    DFSSOLVER<RubikCube3DArray,Hash3d> dfssolver(cube,4);
+    dfssolver.solve();
     return 0;
 }
