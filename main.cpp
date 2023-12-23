f1f1ac2414d523f4264f83f694092ef8f04b1c90
Shreyas Saxena <shreyasflash52@gmail.com>
2023-12-23 14:59:02 +0530

    Updated GenericCube

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 int main() {
     RubikCube3DArray cube;
-    vector<GenericRubikCube::MOVE> shuffle_moves = cube.randomShuffleCube(4);
+    vector<GenericRubikCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
     for(auto move:shuffle_moves)cout<<cube.getMove(move)<<" ";
     cout<<"\n";
     cube.print();
     cout<<"\n";
-    IDDFSSOLVER<RubikCube3DArray,Hash3d> iddfssolver(cube,4);
+    IDDFSSOLVER<RubikCube3DArray,Hash3d> iddfssolver(cube,6);
     vector<GenericRubikCube::MOVE> solving_moves = iddfssolver.solve();
     for(auto move:solving_moves)cout<<cube.getMove(move)<<" ";
     return 0;
