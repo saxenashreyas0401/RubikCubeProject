f356c8f48cd205f984bb936458cee884e03cb6c8
Shreyas Saxena <shreyasflash52@gmail.com>
2023-12-24 14:06:07 +0530

    Updated GenericCube

---
+++ IDASTARSOLVER.h
@@ -0,0 +1,103 @@
+//
+// Created by Shreyas on 24-12-2023.
+//
+
+#ifndef RUBIKCUBEPROJECT_IDASTARSOLVER_H
+#define RUBIKCUBEPROJECT_IDASTARSOLVER_H
+#include<bits/stdc++.h>
+#include "GenericRubikCube.h"
+
+template<typename T, typename H>
+class IDASTARSOLVER{
+private:
+    unordered_map<T, int, H> vis;
+    unordered_map<T, GenericRubikCube::MOVE, H> previous_move;
+    vector<GenericRubikCube::MOVE> moves;
+    struct Node{
+        int depth;
+        int estimate;
+        T cube;
+        Node(T _cube, int _depth, int _estimate) : cube(_cube), depth(_depth), estimate(_estimate) {};
+    };
+    struct compare{
+       bool operator()(pair<Node,int> const &p1, pair<Node,int> const &p2){
+           auto node1 = p1.first; auto node2 = p2.first;
+           if(node1.depth+node1.estimate==node2.depth+node2.estimate)
+           {
+               return node1.estimate>node2.estimate;
+           }
+           else return node1.depth+node1.estimate>node2.depth+node2.estimate;
+       }
+    };
+    void reset()
+    {
+        moves.clear();
+        previous_move.clear();
+        vis.clear();
+    }
+    pair<T,int> IDAstar(int bound)
+    {
+        priority_queue<pair<Node,int>, vector<pair<Node,int>>, compare> pq;
+        auto start = Node(rubiksCube,0,0);
+        pq.push({start,0});
+        int next_bound = 100;
+        while(!pq.empty())
+        {
+            auto p = pq.top();
+            pq.pop();
+            Node cur_node = p.first;
+            previous_move[cur_node.cube] = GenericRubikCube::MOVE(p.second);
+            if(cur_node.cube.isSolved())return {cur_node.cube,bound};
+            if(vis[cur_node.cube])continue;
+            vis[cur_node.cube]=1;
+            cur_node.depth++;
+            for(int i=0;i<18;i++)
+            {
+                auto cur_move = GenericRubikCube::MOVE(i);
+                cur_node.cube.move(cur_move);
+                if(!vis[cur_node.cube])
+                {
+                    cur_node.estimate = 0;
+                    if(cur_node.depth+cur_node.estimate>bound)
+                    {
+                        next_bound = min(next_bound,cur_node.depth+cur_node.estimate);
+                    }
+                    else
+                    {
+                        pq.push({cur_node,i});
+                    }
+                    cur_node.cube.invert(cur_move);
+                }
+            }
+        }
+        return {rubiksCube,next_bound};
+    }
+
+public:
+    T rubiksCube;
+    IDASTARSOLVER(T rc)
+    {
+        rubiksCube = rc;
+    }
+    vector<GenericRubikCube::MOVE> solve(){
+        int bound = 1;
+        auto p = IDAstar(bound);
+        while(p.second!=bound)
+        {
+            reset();
+            bound = p.second;
+            p = IDAstar(bound);
+        }
+        T cur_cube = p.first;
+        while(!(cur_cube==rubiksCube))
+        {
+            moves.push_back(previous_move[cur_cube]);
+            cur_cube.invert(previous_move[cur_cube]);
+        }
+        reverse(moves.begin(),moves.end());
+        return moves;
+    }
+
+};
+
+#endif //RUBIKCUBEPROJECT_IDASTARSOLVER_H
--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include "RubikCube3DArray.cpp"
 #include "DFSSOLVER.h"
 #include "IDDFSSOLVER.h"
+#include "IDASTARSOLVER.h"
 using namespace std;
 int main() {
     RubikCube3DArray cube;
-    vector<GenericRubikCube::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+    vector<GenericRubikCube::MOVE> shuffle_moves = cube.randomShuffleCube(7);
     for(auto move:shuffle_moves)cout<<cube.getMove(move)<<" ";
     cout<<"\n";
     cube.print();
     cout<<"\n";
-    IDDFSSOLVER<RubikCube3DArray,Hash3d> iddfssolver(cube,6);
-    vector<GenericRubikCube::MOVE> solving_moves = iddfssolver.solve();
+    IDASTARSOLVER<RubikCube3DArray,Hash3d> idastarsolver(cube);
+    vector<GenericRubikCube::MOVE> solving_moves = idastarsolver.solve();
     for(auto move:solving_moves)cout<<cube.getMove(move)<<" ";
     return 0;
 }
